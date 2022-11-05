b03f6e953b05c670e40a36a3c5088d30f24ccdc8
Hartz2 <80923471+Hartz2@users.noreply.github.com>
2022-11-04 19:58:14 -0700

    Create teams3.php

---
+++ teams3.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "prescott";
+$password = "embryriddle";
+$dbname = "hartz2_db";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully\n";
+}
+// Run a query
+$sql = "CALL playerTeam('Cletis')";
+$result = $conn->query($sql);
+// Display the resulting table
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row["Team"] . "\n";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
