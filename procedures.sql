b221e273cf225c8745c994314d20728eea51616e
Hartz2 <80923471+Hartz2@users.noreply.github.com>
2022-11-05 17:39:43 -0700

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -19,4 +19,19 @@ END $$
 DELIMITER $$
 CALL playerTeam('Cletis') $$
 
+/************************************************************************************************************************************************************************/
 
+DELIMITER $$
+CREATE PROCEDURE get_schedule(name varchar(20))
+BEGIN
+SELECT Player, when_game AS be_there, Verses.Host AS snack,
+Verses.Team AS against, Verses.Coach AS lead_by
+FROM Players JOIN Schedule ON Schedule.Team = Players.Team
+JOIN Verses ON Verses.Host != Schedule.Host
+AND Verses.GameID = Schedule.GameID
+JOIN Games ON Games.GameID = Verses.GameID 
+WHERE Player = name;
+END $$
+
+DELIMITER $$
+CALL get_schedule('Cletis') $$
